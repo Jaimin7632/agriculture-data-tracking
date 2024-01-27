43a0d1173d17d0de6f30a4212c25f53570a8fa87
Jaimin Patel <jaiminpatel9192@gmail.com>
2024-01-27 16:40:14 +0530

    start location

--- config/global.php
+++ config/global.php
@@ -1,11 +1,11 @@
 <?php
-   
+
 return [
           'soilSensor' => ['key' => 'soilSensorValue', 'type' => 'multi', 'color' => '#FF5733'],
           'pressureSensor' => ['key' => 'pressureSensorValue', 'type' => 'multi', 'color' => '#33FF57'],
           'humiditySensor' => ['key' => 'humiditySensorValue', 'type' => 'multi', 'color' => '#5733FF'],
           'temperatureSensor' => ['key' => 'temperatureSensorValue', 'type' => 'multi', 'color' => '#FF33C7'],
-          // 'location' => ['key' => 'temperatureSensorValue', 'type' => 'single', 'color' => '#E31A1A'],
+          'location' => ['key' => 'temperatureSensorValue', 'type' => 'single', 'color' => '#E31A1A'],
           // Add more sensors as needed
       ]
 ?>
