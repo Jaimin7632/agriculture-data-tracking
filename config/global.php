cbc53df56f58f48b420472aa967dfb4590483814
kiranp <kdpatel1110@gmail.com>
2024-02-19 20:36:06 +0530

    svg image with color

--- config/global.php
+++ config/global.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'soilSensor' => ['key' => 'soilSensorValue', 'type' => 'multi', 'color' => '#FF5733', 'unit' => '% Wetness', 'icon'=> 'assets/img/icons/sensor/soil-green-nature-svgrepo-com.svg'],
-    'pressureSensor' => ['key' => 'pressureSensorValue', 'type' => 'multi', 'color' => '#33FF57', 'unit' => 'Pa', 'icon'=> 'assets/img/icons/sensor/pressure-alt-svgrepo-com.svg'],
-    'humiditySensor' => ['key' => 'humiditySensorValue', 'type' => 'multi', 'color' => '#5733FF', 'unit' => '%', 'icon'=> 'assets/img/icons/sensor/humidity-svgrepo-com.svg'],
-    'temperatureSensor' => ['key' => 'temperatureSensorValue', 'type' => 'multi', 'color' => '#FF33C7', 'unit' => '°C', 'icon'=> 'assets/img/icons/sensor/temperature-list-svgrepo-com.svg'],
+    'soilSensor' => ['key' => 'soilSensorValue', 'type' => 'multi', 'color' => '#FF5733', 'unit' => '% Wetness', 'icon'=> 'assets/img/icons/sensor/soilsvg.svg'],
+    'pressureSensor' => ['key' => 'pressureSensorValue', 'type' => 'multi', 'color' => '#33FF57', 'unit' => 'Pa', 'icon'=> 'assets/img/icons/sensor/svgpressure.svg'],
+    'humiditySensor' => ['key' => 'humiditySensorValue', 'type' => 'multi', 'color' => '#5733FF', 'unit' => '%', 'icon'=> 'assets/img/icons/sensor/svghumidity.svg'],
+    'temperatureSensor' => ['key' => 'temperatureSensorValue', 'type' => 'multi', 'color' => '#FF33C7', 'unit' => '°C', 'icon'=> 'assets/img/icons/sensor/svgtempature.svg'],
     'location' => ['key' => 'location', 'type' => 'single', 'color' => '#E31A1A', 'unit' => '', 'icon'=> 'menu-icon tf-icons bx bx-detail']
 ]
 ?>
