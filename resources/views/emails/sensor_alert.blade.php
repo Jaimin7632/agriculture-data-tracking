ec1ea954fc1f082dd1d3bdc09b4e5bfb81e97195
kiranp <kdpatel1110@gmail.com>
2024-05-15 20:51:38 +0530

    http to https redirection

--- resources/views/emails/sensor_alert.blade.php
+++ resources/views/emails/sensor_alert.blade.php
@@ -8,8 +8,9 @@
     <p>The sensor value for {{ $sensorName }} has exceeded the defined thresholds:</p>
     <p>Minimum Value: {{ $minValue }}</p>
     <p>Maximum Value: {{ $maxValue }}</p>
+    <p>Actual Value: {{ $actualValue }}</p>
     <p>Please take necessary action.</p>
     <p>Thank you!</p>
-    <p>Regards, Your Application</p>
+    <p>Regards, Iot Embeded</p>
 </body>
 </html>
