546d54cd34e89b868d95f8e3b22f9a6eeaa703e9
Jaimin Patel <jaiminpatel9192@gmail.com>
2024-05-26 10:11:09 +0530

    .

--- Hardware Code/I2C and lora/tsim/tsim.ino
+++ Hardware Code/I2C and lora/tsim/tsim.ino
@@ -11,6 +11,13 @@ void setup() {
 String receiveJsonString() {
   String jsonResponse = "";
   Wire.requestFrom(8, 500); // Request more than necessary
+  for (int attempts = 0; attempts < MAX_ATTEMPTS; attempts++) {
+    if (Wire.available() >= 2) {
+      break;
+    }
+    Wire.requestFrom(8, 500);
+    delay(500);
+  }
   bool endReached = false;
 
   while (Wire.available() && !endReached) {
